44230babe130a5bed07fb94697599b3dd1b39c40
Samuel Berwanger <samuelbwr@gmail.com>
2018-05-16 00:03:21 +0200

    setting up front end with react js

--- back/src/main/java/ie/samuelbwr/todolist/TodolistApplication.java
+++ back/src/main/java/ie/samuelbwr/todolist/TodolistApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodolistApplication  extends SpringBootServletInitializer {
+public class TodolistApplication extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure( SpringApplicationBuilder application) {
