ff77cffddc054ec48822d30020649a2c6c2e46e5
Samuel Berwanger <samuelbwr@gmail.com>
2018-05-17 11:10:02 +0200

    fixing routing

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,60 @@
 import React from 'react';
 import Login from './pages/Login';
+import TodoList from './pages/TodoList';
+import Grid from '@material-ui/core/Grid';
 
-function App(props){
-    console.log(props);
-    if(props.isLoggedIn)
-      return <div>Logged in</div>;
-    return <Login/>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentUser: null, isAuthenticated: true, isLoading: false };
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.loadSessionUser = this.loadSessionUser.bind(this);
+  }
+
+  loadSessionUser = () => {
+    this.setState(null, false, true);
+    // getSessionUser()
+    // .then( user => {
+    //   this.handleAuthentication(user);
+    // }).catch(error => {
+    //   this.handleAuthentication(null);
+    // })
+  }
+
+  handleAuthentication = (user) => {
+    if(user !==  null)
+      this.setState(user, true, false)
+    else
+      this.setState(null, false, false)
+  }
+
+  
+  setState = (user, isAuthenticated, isLoading) =>{
+    this.setState(() => ({
+      currentUser: user,
+      isAuthenticated: isAuthenticated,
+      isLoading: isLoading
+    }))
+  }
+
+  getCurrentPage(){
+    if(this.state.isAuthenticated)
+      return <TodoList/>;
+  
+    return <Login onAuthentication={this.handleAuthentication}/>
+  }
+
+  render(){
+  return (
+    <Grid container 
+      className="App-container"
+      alignItems="center"
+      justify="center">
+      <Grid item lg={6} className="App-container-item">
+        {this.getCurrentPage()}
+      </Grid>
+    </Grid>
+  )}
 }
 
 export default App;
